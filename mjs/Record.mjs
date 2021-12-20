859fbf530ec83d14cc1b602baa3a02d9770963fe
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-20 12:40:15 +0900

    Update Record.mjs

--- mjs/Record.mjs
+++ mjs/Record.mjs
@@ -1,23 +1,20 @@
-// https://qiita.com/optimisuke/items/f1434d4a46afd667adc6
 export class Record {
-    constructor({ctx, ch = 2, bufSize = 0}){
-        this.ch = ch;
+    constructor(ctx, ch = 2, bufSize = 0){
+        this.closed = false;
+        this.data = [...Array(ch).fill([])];
         this.node = ctx.createScriptProcessor(bufSize, ch, ch);
         this.node.onaudioprocess = e => this.process(e);
-        this.sampleRate = ctx.sampleRate;
-        this.bufs = [];
-        this.closed = false;
     }
     close(){
         this.closed = true;
     }
     process({inputBuffer, outputBuffer}){
-        if(this.closed) return;
-        const {channelCount} = this.node;
-        for(let i = 0; i < channelCount; i++) {
+        const {closed, data} = this;
+        if(closed) return;
+        for(const i of data.keys()) {
             const buf = inputBuffer.getChannelData(i);
-            this.bufs.push(buf.slice());
             outputBuffer.getChannelData(i).set(buf);
+            data[i].push(buf.slice());
         }
     }
 }
