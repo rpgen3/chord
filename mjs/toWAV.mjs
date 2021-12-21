8a6f4e8ab13429735499e52779c4f16cc634ac8c
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-21 12:24:33 +0900

    Update toWAV.mjs

--- mjs/toWAV.mjs
+++ mjs/toWAV.mjs
@@ -1,6 +1,6 @@
 // https://qiita.com/optimisuke/items/f1434d4a46afd667adc6
-export const toWAV = (data, sampleRate) => {
-    const view = makeFile(mergeBuf(data), data.length, sampleRate),
+export const toWAV = (data, sampleRate, bitRate) => {
+    const view = makeFile(mergeBuf(data), data.length, sampleRate, bitRate),
           blob = new Blob([view], {type:'audio/wav'});
     return URL.createObjectURL(blob);
 };
@@ -13,22 +13,23 @@ const mergeBuf = data => {
     return a;
 };
 // https://www.wdic.org/w/TECH/WAV
-const makeFile = (a, ch, sampleRate) => {
-    const view = new DataView(new ArrayBuffer(44 + a.length * 2));
+const makeFile = (a, ch, sampleRate, bitRate) => {
+    const len = a.length * 2,
+          view = new DataView(new ArrayBuffer(44 + len));
     writeString(view, 0, 'RIFF'); // RIFFヘッダ
-    view.setUint32(4, 32 + a.length * 2, true); // これ以降のファイルサイズ
+    view.setUint32(4, 32 + len, true); // これ以降のファイルサイズ
     writeString(view, 8, 'WAVE'); // WAVEヘッダ
     writeString(view, 12, 'fmt '); // fmtチャンク
     view.setUint32(16, 16, true); // fmtチャンクのバイト数
     view.setUint16(20, 1, true); // フォーマットID
     view.setUint16(22, ch, true); // チャンネル数
     view.setUint32(24, sampleRate, true); // サンプリングレート
-    view.setUint32(28, sampleRate * 2 * ch, true); // データ速度
-    view.setUint16(32, 2 * ch, true); // ブロックサイズ
-    view.setUint16(34, 16, true); // サンプルあたりのビット数
+    view.setUint32(28, sampleRate * ch * 2, true); // データ速度
+    view.setUint16(32, ch * 2, true); // ブロックサイズ
+    view.setUint16(34, bitRate, true); // サンプルあたりのビット数
     writeString(view, 36, 'data'); // dataチャンク
-    view.setUint32(40, a.length * 2, true); // 波形データのバイト数
-    floatTo16BitPCM(view, 44, a); // 波形データ
+    view.setUint32(40, len, true); // 波形データのバイト数
+    float2PCM(view, 44, a, bitRate); // 波形データ
     return view;
 };
 const writeString = (view, offset, string) => {
@@ -36,9 +37,20 @@ const writeString = (view, offset, string) => {
         view.setUint8(offset + i, string.charCodeAt(i));
     }
 };
-const floatTo16BitPCM = (output, offset, input) => {
-    for (let i = 0; i < input.length; i++ , offset += 2) {
+const float2PCM = (output, offset, input, bitRate) => {
+    const step = 32 / bitRate;
+    for (let i = 0; i < input.length; i++ , offset += step) {
         const s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+        switch(bitRate) {
+            case 8:
+                output.setUint8(offset, (s + 1) * (0xFF / 2) | 0);
+                break;
+            case 16:
+                output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+                break;
+            case 32:
+                output.setFloat32(offset, input[i], true);
+                break;
+        }
     }
 };
