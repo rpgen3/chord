882016aae62858da5d5d4bdc0b8044b7c86c68e5
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-21 15:06:16 +0900

    Update toWAV.mjs

--- mjs/toWAV.mjs
+++ mjs/toWAV.mjs
@@ -1,23 +1,23 @@
 // https://qiita.com/optimisuke/items/f1434d4a46afd667adc6
 export const toWAV = ({data, sampleRate, bitRate = 16}) => {
     if(![8, 16, 24, 32].includes(bitRate)) throw 'BitRate must be 8 or 16 or 24 or 32.';
-    const view = makeFile(mergeBuf(data), data.length, sampleRate, bitRate),
+    const view = makeFile(mergeData(data), data.length, sampleRate, bitRate),
           blob = new Blob([view], {type:'audio/wav'});
     return URL.createObjectURL(blob);
 };
-const mergeBuf = data => {
+const mergeData = data => {
     const ch = data.length,
           len = data[0].length,
           bufSize = data[0][0].length,
-          a = new Float32Array(ch * len * bufSize);
-    for (let i = 0; i < ch; i++) for (let j = 0; j < len; j++) a.set(data[i][j], (i + j * ch) * bufSize);
-    return a;
+          arr = new Float32Array(ch * len * bufSize);
+    for (let i = 0; i < ch; i++) for (let j = 0; j < len; j++) arr.set(data[i][j], (i + j * ch) * bufSize);
+    return arr;
 };
 // https://www.wdic.org/w/TECH/WAV
-const makeFile = (a, ch, sampleRate, bitRate) => {
+const makeFile = (arr, ch, sampleRate, bitRate) => {
     const step = bitRate / 8,
           blockSize = ch * step,
-          len = a.length * step,
+          len = arr.length * step,
           view = new DataView(new ArrayBuffer(44 + len));
     writeString(view, 0, 'RIFF'); // RIFFヘッダ
     view.setUint32(4, 32 + len, true); // これ以降のファイルサイズ
@@ -32,7 +32,7 @@ const makeFile = (a, ch, sampleRate, bitRate) => {
     view.setUint16(34, bitRate, true); // サンプルあたりのビット数
     writeString(view, 36, 'data'); // dataチャンク
     view.setUint32(40, len, true); // 波形データのバイト数
-    float2pcm(view, 44, a, step); // 波形データ
+    float2pcm(view, 44, arr, step); // 波形データ
     return view;
 };
 const writeString = (view, offset, string) => {
@@ -40,9 +40,9 @@ const writeString = (view, offset, string) => {
         view.setUint8(offset + i, string.charCodeAt(i));
     }
 };
-const float2pcm = (view, offset, input, step) => {
+const float2pcm = (view, offset, arr, step) => {
     const f = [pcm8, pcm16, pcm24, pcm32][step - 1];
-    for (let i = 0; i < input.length; i++ , offset += step) f(view, offset, input[i]);
+    for (let i = 0; i < arr.length; i++ , offset += step) f(view, offset, arr[i]);
 };
 // https://github.com/mohayonao/wav-encoder/blob/master/index.js
 const clamp = (num, min, max) => Math.max(min, Math.min(max, num)),
