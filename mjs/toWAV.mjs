5714adb4c0e079c2198185375b740cdffd7ac9e3
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-21 14:24:35 +0900

    Update toWAV.mjs

--- mjs/toWAV.mjs
+++ mjs/toWAV.mjs
@@ -1,6 +1,6 @@
 // https://qiita.com/optimisuke/items/f1434d4a46afd667adc6
 export const toWAV = ({data, sampleRate, bitRate = 16}) => { // bitRate: 8 or 16 or 32
-    if(bitRate !== 8 && bitRate !== 16 && bitRate !== 32) throw 'BitRate must be 8 or 16 or 32.';
+    if(![8, 16, 24, 32].includes(bitRate)) throw 'BitRate must be 8 or 16 or 24 or 32.';
     const view = makeFile(mergeBuf(data), data.length, sampleRate, bitRate),
           blob = new Blob([view], {type:'audio/wav'});
     return URL.createObjectURL(blob);
@@ -15,7 +15,9 @@ const mergeBuf = data => {
 };
 // https://www.wdic.org/w/TECH/WAV
 const makeFile = (a, ch, sampleRate, bitRate) => {
-    const len = a.length * 2,
+    const step = bitRate / 8,
+          blockSize = ch * step,
+          len = a.length * step,
           view = new DataView(new ArrayBuffer(44 + len));
     writeString(view, 0, 'RIFF'); // RIFFヘッダ
     view.setUint32(4, 32 + len, true); // これ以降のファイルサイズ
@@ -25,12 +27,12 @@ const makeFile = (a, ch, sampleRate, bitRate) => {
     view.setUint16(20, 1, true); // フォーマットID
     view.setUint16(22, ch, true); // チャンネル数
     view.setUint32(24, sampleRate, true); // サンプリングレート
-    view.setUint32(28, sampleRate * ch * 2, true); // データ速度
-    view.setUint16(32, ch * 2, true); // ブロックサイズ
+    view.setUint32(28, sampleRate * blockSize, true); // データ速度
+    view.setUint16(32, blockSize, true); // ブロックサイズ
     view.setUint16(34, bitRate, true); // サンプルあたりのビット数
     writeString(view, 36, 'data'); // dataチャンク
     view.setUint32(40, len, true); // 波形データのバイト数
-    float2PCM(view, 44, a, bitRate); // 波形データ
+    float2pcm(view, 44, a, step); // 波形データ
     return view;
 };
 const writeString = (view, offset, string) => {
@@ -38,20 +40,19 @@ const writeString = (view, offset, string) => {
         view.setUint8(offset + i, string.charCodeAt(i));
     }
 };
-const float2PCM = (output, offset, input, bitRate) => {
-    const step = bitRate / 8;
-    for (let i = 0; i < input.length; i++ , offset += step) {
-        const s = Math.max(-1, Math.min(1, input[i]));
-        switch(bitRate) {
-            case 8:
-                output.setUint8(offset, (s + 1) * (0xFF / 2) | 0);
-                break;
-            case 16:
-                output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-                break;
-            case 32:
-                output.setFloat32(offset, input[i], true);
-                break;
-        }
-    }
+const float2pcm = (view, offset, input, step) => {
+    const f = [pcm8, pcm16, pcm24, pcm32][step - 1];
+    for (let i = 0; i < input.length; i++ , offset += step) f(view, offset, input[i]);
+};
+// https://github.com/mohayonao/wav-encoder/blob/master/index.js
+const clamp = (num, min, max) => Math.max(min, Math.min(max, num)),
+      float2Int = (value, range) => clamp(Math.round(value * range), -range, range - 1),
+      pcm8 = (view, offset, value) => view.setUint8(offset, float2Int(value, 0x80) + 0x80, true),
+      pcm16 = (view, offset, value) => view.setInt16(offset, float2Int(value, 0x8000), true),
+      pcm32 = (view, offset, value) => view.setInt32(offset, float2Int(value, 0x80000000), true);
+const pcm24 = (view, offset, value) => {
+    const v = float2Int(value, 800000);
+    view.setUint8(offset + 0, (v >>  0) & 0xFF);
+    view.setUint8(offset + 1, (v >>  8) & 0xFF);
+    view.setUint8(offset + 2, (v >>  16) & 0xFF);
 };
