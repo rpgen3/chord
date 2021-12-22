d85639acedeebd97344741ec7acb592abe0935a3
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-22 22:38:43 +0900

    Create RecordWorklet.mjs

---
+++ mjs/RecordWorklet.mjs
@@ -0,0 +1,21 @@
+export class RecordWorklet {
+    constructor({ctx, ch = 2}){
+        this.node = new AudioWorkletNode(ctx, 'Record', {
+            processorOptions: {ch}
+        });
+    }
+    aysnc init(ctx){
+        await ctx.audioWorklet.addModule('https://rpgen3.github.io/chord/worklet/Record.js');
+    }
+    close(){
+        this.node.port.postMessage(0);
+    }
+    async getData(){
+        return new Promise(resolve => {
+            this.node.port.onmessage = ({data}) => {
+                resolve(data);
+            };
+            this.node.port.postMessage(1);
+        });
+    }
+}
