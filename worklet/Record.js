ca66b07e93c272cf376a31b99508798056405e8d
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-22 23:35:40 +0900

    Update Record.js

--- worklet/Record.js
+++ worklet/Record.js
@@ -2,15 +2,17 @@ class Record extends AudioWorkletProcessor {
     constructor(options) {
         super(options);
         this.closed = false;
-        this.data = [...Array(options.processorOptions.ch).fill().map(v => [])];
+        this.data = [...Array(options.channelCount).fill().map(v => [])];
         this.port.onmessage = ({data}) => {
             if(data === 0) this.closed = true;
             else if(data === 1) this.port.postMessage(this.data);
         };
     }
-    process([input], [output], parameters) {
+    process(inputs, outputs, parameters) {
         const {closed, data} = this;
         if(closed) return false;
+        const input = inputs[0],
+              output = outputs[0];
         for(const i of data.keys()) {
             const buf = input[i];
             output[i].set(buf);
