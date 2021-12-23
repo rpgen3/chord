9fc219bac1b20be50c2c1dc68da7fc371cbc136e
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-23 15:11:11 +0900

    Update Fork.js

--- worklet/Fork.js
+++ worklet/Fork.js
@@ -4,8 +4,8 @@ class Fork extends AudioWorkletProcessor {
         this.ch = options.outputChannelCount[0];
     }
     process([input], outputs) {
-        const max = Math.max(input.length, this.ch);
-        for(let i = 0; i < max; i++) for(const output of outputs) output[i].set(input[i]);
+        const min = Math.min(input.length, this.ch);
+        for(let i = 0; i < min; i++) for(const output of outputs) output[i].set(input[i]);
         return true;
     }
 }
