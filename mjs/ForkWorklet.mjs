22e542966612a6341702e756f748f563e013950f
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-23 10:22:18 +0900

    Create ForkWorklet.mjs

---
+++ mjs/ForkWorklet.mjs
@@ -0,0 +1,12 @@
+export class ForkWorklet {
+    static init(ctx){
+        return ctx.audioWorklet.addModule('https://rpgen3.github.io/chord/worklet/Fork.js');
+    }
+    constructor({ctx, ch = 2}){
+        this.node = new AudioWorkletNode(ctx, 'Fork', {
+            numberOfInputs: 1,
+            numberOfOutputs: 2,
+            outputChannelCount: [ch, ch]
+        });
+    }
+}
