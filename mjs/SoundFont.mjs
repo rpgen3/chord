ab18c9015da9986c4f7778a3f55b12c95ebaa43a
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-23 13:01:25 +0900

    Update SoundFont.mjs

--- mjs/SoundFont.mjs
+++ mjs/SoundFont.mjs
@@ -2,6 +2,12 @@ import {getScript} from 'https://rpgen3.github.io/mylib/export/import.mjs';
 import {flat2sharp} from 'https://rpgen3.github.io/chord/mjs/flat2sharp.mjs';
 if(!('MIDI' in window)) window.MIDI = {};
 if(!('Soundfont' in window.MIDI)) window.MIDI.Soundfont = {};
+class Font {
+    constructor({bufs, ch}){
+        this.bufs = bufs;
+        this.ch = ch;
+    }
+}
 export class SoundFont {
     static ctx = null;
     static fonts = new Map;
@@ -15,38 +21,34 @@ export class SoundFont {
         if(!(fontName in Soundfont)) await getScript(url);
         if(!(fontName in Soundfont)) throw 'SoundFont is not found.';
         const {fonts} = this;
-        if(fonts.has(fontName)) return fonts.get(fontName);
-        else {
-            const font = new Map(await Promise.all(
+        if(!fonts.has(fontName)) {
+            let ch = -1;
+            const bufs = new Map(await Promise.all(
                 Object.entries(window.MIDI.Soundfont[fontName]).map(async ([k, v]) => {
                     const res = await fetch(v),
                           buf = await res.arrayBuffer(),
-                          _buf = await SoundFont.ctx.decodeAudioData(buf);
+                          _buf = await SoundFont.ctx.decodeAudioData(buf),
+                          {numberOfChannels} = _buf;
+                    if(ch < numberOfChannels) ch = numberOfChannels;
                     return [flat2sharp(k), _buf];
                 })
             ));
-            fonts.set(fontName, font);
-            return font;
+            fonts.set(fontName, new Font({bufs, ch}));
         }
-    }
-    static getCh(font){
-        let ch = -1;
-        for(const {numberOfChannels} of font) if(ch < numberOfChannels) ch = numberOfChannels;
-        return ch;
+        return fonts.get(fontName);
     }
     constructor(isDrum = false){
         this.font = null;
-        this.ch = -1;
         this.isDrum = isDrum;
         this.min = 0.5;
     }
     async load(fontName, url){ // https://github.com/gleitz/midi-js-soundfonts
         this.font = await SoundFont.load(fontName, url);
-        this.ch = SoundFont.getCh(this.font);
     }
     play(note = 'C4', volume = 1.0, duration = 1.0){
-        if(!this.font.has(note)) return;
-        const buf = this.font.get(note),
+        const {bufs} = this.font;
+        if(!bufs.has(note)) return;
+        const buf = bufs.get(note),
               {ctx, anyNode} = SoundFont,
               src = ctx.createBufferSource(),
               g = ctx.createGain();
