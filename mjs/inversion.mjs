fe259834cd578da3ad4f00741bb00fa2daf988af
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-12-30 23:14:09 +0900

    Update inversion.mjs

--- mjs/inversion.mjs
+++ mjs/inversion.mjs
@@ -3,7 +3,8 @@ export const inversion = ( // 和音の転回
     diff = +1 // 転回指数
 ) => {
     const a = chord.slice();
-    if(diff > 0) while(diff--) a.push(a.shift() + 12);
+    if(diff === 0) return a;
+    else if(diff > 0) while(diff--) a.push(a.shift() + 12);
     else while(diff++) a.unshift(a.pop() - 12);
     return a;
 };
