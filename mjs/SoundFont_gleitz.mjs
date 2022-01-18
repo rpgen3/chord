ca990139600c85fe6bd3a5eff1f80e4a83e03012
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-01-18 09:33:07 +0900

    Update SoundFont_gleitz.mjs

--- mjs/SoundFont_gleitz.mjs
+++ mjs/SoundFont_gleitz.mjs
@@ -1,6 +1,6 @@
 // https://github.com/gleitz/midi-js-soundfonts/
 import {getScript} from 'https://rpgen3.github.io/mylib/export/import.mjs';
-import {flat2sharp} from 'https://rpgen3.github.io/chord/mjs/flat2sharp.mjs';
+import {flat2sharp} from 'https://rpgen3.github.io/soundfont/mjs/flat2sharp.mjs';
 if(!('MIDI' in window)) window.MIDI = {};
 if(!('Soundfont' in window.MIDI)) window.MIDI.Soundfont = {};
 export class SoundFont_gleitz {
