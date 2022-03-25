c78979bdc90b2dfe1b2cfdef292209adc2177f52
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-03-25 17:24:23 +0900

    Delete flat2sharp.mjs

--- mjs/flat2sharp.mjs
+++
@@ -1,7 +0,0 @@
-const key = 'ABCDEFG',
-      reg = /^([ABDEG])b([0-9])$/;
-export const flat2sharp = note => {
-    if(!reg.test(note)) return note;
-    const [a, b, c] = note.match(reg);
-    return key[(key.indexOf(b) + key.length - 1) % key.length] + '#' + c;
-};
