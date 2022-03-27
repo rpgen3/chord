845cb124b50ef6765fa39c170bfb757ef6133789
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-03-27 15:25:03 +0900

    Update SoundFont.mjs

--- mjs/gleitz/SoundFont.mjs
+++ mjs/gleitz/SoundFont.mjs
@@ -1,41 +1,47 @@
-// https://github.com/surikov/webaudiofontdata/
+// https://github.com/gleitz/midi-js-soundfonts/
 import {getScript} from 'https://rpgen3.github.io/mylib/export/import.mjs';
+import {flat2sharp} from 'https://rpgen3.github.io/midi/mjs/flat2sharp.mjs';
+import {piano} from 'https://rpgen3.github.io/midi/mjs/piano.mjs';
+if(!('MIDI' in window)) window.MIDI = {};
+if(!('Soundfont' in window.MIDI)) window.MIDI.Soundfont = {};
 export class SoundFont {
-    static afterTime = 0.4; // echo?
     static fonts = new Map;
     static ch = -1;
     static toURL(fontName){
-        return `https://surikov.github.io/webaudiofontdata/sound/${fontName}.js`;
+        return `https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/${fontName}-mp3.js`;
     }
     static async load({
         ctx,
         fontName,
         url,
-        isDrum = false,
-        pitchs
+        isDrum = false
     }){
-        if(!(fontName in window)) await getScript(url);
-        if(!(fontName in window)) throw 'SoundFont is not found.';
+        const {Soundfont} = window.MIDI;
+        if(!(fontName in Soundfont)) await getScript(url);
+        if(!(fontName in Soundfont)) throw 'SoundFont is not found.';
         const {fonts} = this;
         if(!fonts.has(fontName)) {
-            const zones = new Map;
             let ch = -1;
-            for(const [pitch, v] of (
-                await findZone(ctx, window[fontName].zones, pitchs)
-            )) {
-                const {numberOfChannels} = v.buffer;
-                if(ch < numberOfChannels) ch = numberOfChannels;
-                zones.set(Number(pitch), v);
-            }
+            const bufs = new Map(await Promise.all(
+                Object.entries(window.MIDI.Soundfont[fontName]).map(async ([k, v]) => {
+                    const res = await fetch(v),
+                          buf = await res.arrayBuffer(),
+                          _buf = await ctx.decodeAudioData(buf),
+                          {numberOfChannels} = _buf;
+                    if(ch < numberOfChannels) ch = numberOfChannels;
+                    return [piano.note2index(flat2sharp(k)) + 21, _buf];
+                })
+            ));
             if(this.ch < ch) this.ch = ch;
-            fonts.set(fontName, new this(zones, ch, isDrum));
+            fonts.set(fontName, new this(bufs, ch, isDrum));
         }
         return fonts.get(fontName);
     }
-    constructor(zones, ch, isDrum){
-        this.zones = zones;
+    constructor(bufs, ch, isDrum){
+        this.bufs = bufs;
         this.ch = ch;
         this.isDrum = isDrum;
+        this.min = 0.5;
     }
     play({
         ctx = new AudioContext,
@@ -45,96 +51,18 @@ export class SoundFont {
         when = 0.0,
         duration = 1.0
     }={}){
-        const {zones, isDrum} = this;
-        if(!zones.has(pitch)) return;
-        const zone = zones.get(pitch),
+        const {bufs, isDrum} = this;
+        if(!bufs.has(pitch)) return;
+        const buf = bufs.get(pitch),
               src = ctx.createBufferSource(),
               g = ctx.createGain(),
               _when = when + ctx.currentTime,
-              {buffer, _param} = zone;
-        src.buffer = buffer;
+              end = _when + (isDrum ? buf.duration : Math.min(buf.duration, Math.max(this.min, duration)));
+        src.buffer = buf;
         g.gain.value = volume;
-        src.playbackRate.setValueAtTime(_param.playbackRate, 0);
-        Object.assign(src, _param.src);
-        const _duration = duration + this.constructor.afterTime,
-              end = _when + (isDrum ? buffer.duration : (src.loop ? _duration : Math.min(_duration, _param.min)));
         if(!isDrum) g.gain.linearRampToValueAtTime(0, end);
         src.connect(g).connect(destination);
         src.start(_when);
         src.stop(end);
     }
 }
-const findZone = (ctx, zones, pitchs = []) => {
-    if(!pitchs.length) for(const zone of zones) {
-        const low = zone.keyRangeLow | 0,
-              high = zone.keyRangeHigh | 0;
-        if(low > high) continue;
-        for(let i = low; i <= high; i++) pitchs.push(i);
-    }
-    const set = new Set(pitchs),
-          map = new Map(pitchs.map(v => [v, zones[0]]));
-    for (let i = zones.length - 1; i >= 0; i--) for(const v of set) {
-        const zone = zones[i];
-        if (v < zone.keyRangeLow || v > zone.keyRangeHigh + 1) continue;
-        set.delete(v);
-        map.set(v, {...zone});
-    }
-    return Promise.all([...map].map(async ([k, v]) => {
-        await adjustZone(ctx, v)
-        await addParam(v, k);
-        return [k, v];
-    }));
-};
-const adjustZone = async (ctx, zone) => {
-    if (zone.buffer) return;
-    zone.delay = 0;
-    if (zone.sample) {
-        const decoded = atob(zone.sample);
-        zone.buffer = ctx.createBuffer(1, decoded.length / 2, zone.sampleRate);
-        const a = zone.buffer.getChannelData(0);
-        for (let i = 0; i < decoded.length / 2; i++) {
-            let b1 = decoded.charCodeAt(i * 2),
-                b2 = decoded.charCodeAt(i * 2 + 1);
-            if (b1 < 0) b1 = 0x100 + b1;
-            if (b2 < 0) b2 = 0x100 + b2;
-            let n = b2 * 0x100 + b1;
-            if (n >= 0x10000 / 2) n = n - 0x10000;
-            a[i] = n / 0x10000;
-        }
-    }
-    else if (zone.file) {
-        const buf = Uint8Array.from(atob(zone.file), c => c.charCodeAt()).buffer;
-        zone.buffer = await ctx.decodeAudioData(buf);
-    }
-    for(const [k, v] of [
-        ['loopStart', 0],
-        ['loopEnd', 0],
-        ['coarseTune', 0],
-        ['fineTune', 0],
-        ['originalPitch', 6000],
-        ['sampleRate', 44100],
-        ['sustain', 0]
-    ]) if(Number.isNaN(Number(zone[k]))) zone[k] = v;
-};
-const addParam = (zone, pitch) => {
-    const {
-        originalPitch,
-        keyRangeLow,
-        keyRangeHigh,
-        loopStart,
-        loopEnd,
-        coarseTune,
-        fineTune,
-        sampleRate,
-        delay,
-        buffer
-    } = zone,
-          baseDetune = originalPitch - 100 * coarseTune - fineTune,
-          playbackRate = Math.pow(2, (100 * pitch - baseDetune) / 1200),
-          min = buffer.duration / playbackRate,
-          src = {
-              loop: loopStart >= 1 && loopStart < loopEnd
-          };
-    if(src.loop) [src.loopStart, src.loopEnd] = [loopStart, loopEnd].map(v => v / sampleRate + delay);
-    zone._param = {playbackRate, min, src};
-};
